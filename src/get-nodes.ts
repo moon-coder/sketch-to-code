a8b45a27123a4654824cd1a7c9e044415cebf374
yangxiaodong <yangxiaodong@qianmi.com>
2019-10-18 10:34:36 +0800

    fix:  sketch中原生节点 转换为 node节点时 处理问题...

--- src/get-nodes.ts
+++ src/get-nodes.ts
@@ -10,8 +10,8 @@ import {
 } from './types-sketch';
 import {IFrame, INode} from './types';
 import * as uuid from 'uuid';
-const sketch = require('sketch');
-const Slice = require('sketch/dom').Slice;
+// const sketch = require('sketch');
+// const Slice = require('sketch/dom').Slice;
 
 interface NodeExtraInfo {
   __absFrame: {x: number; y: number};
@@ -27,41 +27,42 @@ interface NodeInfoRepo {
  * @returns {INode[]}
  */
 export default (layer: Layer): INode[] => {
-  const layers: Layer[] = [];
-
   const resultNodes: INode[] = [];
 
   const walk = (
     layer: Layer,
     {
       lv = 0,
-      pPath = '#',
+      pPath = '',
+      relPath="#",
       nodeRepo = {},
-    }: {lv: number; pPath: string; nodeRepo: NodeInfoRepo},
+    }: {lv: number; pPath: string; relPath:string; nodeRepo: NodeInfoRepo},
   ) => {
+    let currentNodePath = `${pPath}${relPath}`;
+
     if (lv != 0 && layer.parent && nodeRepo[pPath]) {
-      let {x: px, y: py} = layer.parent.__absFrame;
+      let {x: px, y: py} = nodeRepo[pPath].__absFrame;
       let {x, y} = layer.frame;
-      nodeRepo[pPath] = {
+      nodeRepo[`${currentNodePath}`] = {
         __absFrame: Object.assign({}, layer.frame, {x: x + px, y: y + py}),
       };
     } else if (lv === 0) {
-      nodeRepo[pPath] = {
+      nodeRepo[`${currentNodePath}`] = {
         __absFrame: Object.assign({}, layer.frame),
       };
     }
 
     // 导出图片
-    exportImg(layer);
+    // exportImg(layer);
 
     if (!['Artboard', 'Group'].includes(layer.type) || layer.__imgSrc) {
-      layers.push(layer);
-      toNode(layer);
+      toNode(layer,nodeRepo[pPath]);
     } else {
       (layer as Container).layers.forEach((layer, layrerIndex) => {
         walk(layer, {
           lv: lv + 1,
-          pPath: pPath + `.layers[${layrerIndex}]`,
+          pPath:`${currentNodePath}`,
+          relPath:`.layers[${layrerIndex}]`,
           nodeRepo,
         });
       });
@@ -86,7 +87,8 @@ export default (layer: Layer): INode[] => {
 
   walk(layer, {
     lv: 0,
-    pPath: '#',
+    pPath: '',
+    relPath:"#",
     nodeRepo: {},
   });
 
@@ -143,58 +145,58 @@ const layerType = (layer: Layer): 'Block' | 'Image' | 'Text' => {
 /**
  * 导出图片
  */
-const exportImg = (layer: Layer) => {
-  let slice = new Slice({
-    frame: {
-      x: 0,
-      y: 0,
-      width: layer.frame.width,
-      height: layer.frame.height,
-    },
-  });
-  // 1.图片的情况
-  if (layer.type === 'Image') {
-    slice.parent = layer.parent;
-    const imgSrc = sliceImg(slice);
-    layer.__imgSrc = imgSrc;
-  }
-  // 2."-合并"的情况
-  if (layer.type === 'Group' && layer.name.endsWith('-合并')) {
-    slice.parent = layer;
-    const imgSrc = sliceImg(slice);
-    layer.__imgSrc = imgSrc;
-  }
-  // 3.背景填充的情况
-  const fill = layer.style.fills[0];
-  if (fill && fill.pattern && fill.pattern.image) {
-    // TODO
-  }
-};
+// const exportImg = (layer: Layer) => {
+//   let slice = new Slice({
+//     frame: {
+//       x: 0,
+//       y: 0,
+//       width: layer.frame.width,
+//       height: layer.frame.height,
+//     },
+//   });
+//   // 1.图片的情况
+//   if (layer.type === 'Image') {
+//     slice.parent = layer.parent;
+//     const imgSrc = sliceImg(slice);
+//     layer.__imgSrc = imgSrc;
+//   }
+//   // 2."-合并"的情况
+//   if (layer.type === 'Group' && layer.name.endsWith('-合并')) {
+//     slice.parent = layer;
+//     const imgSrc = sliceImg(slice);
+//     layer.__imgSrc = imgSrc;
+//   }
+//   // 3.背景填充的情况
+//   const fill = layer.style.fills[0];
+//   if (fill && fill.pattern && fill.pattern.image) {
+//     // TODO
+//   }
+// };
 
 /**
  * 切片导出
  */
-const sliceImg = ((): Function => {
-  let imgIdx = 0;
-  return (slice: Slice): string => {
-    const imgName = `img${imgIdx++}`;
-    slice.name = imgName;
-    slice.exportFormats = [
-      {
-        fileFormat: 'png',
-        prefix: imgName,
-        suffix: 'png',
-        size: '2x',
-      },
-    ];
-    sketch.export(slice, {
-      formats: 'png',
-      output: '/Users/shejijiang/Desktop/temp/img',
-    });
-    slice.remove();
-    return `./img/${imgName}.png`;
-  };
-})();
+// const sliceImg = ((): Function => {
+//   let imgIdx = 0;
+//   return (slice: Slice): string => {
+//     const imgName = `img${imgIdx++}`;
+//     slice.name = imgName;
+//     slice.exportFormats = [
+//       {
+//         fileFormat: 'png',
+//         prefix: imgName,
+//         suffix: 'png',
+//         size: '2x',
+//       },
+//     ];
+//     sketch.export(slice, {
+//       formats: 'png',
+//       output: '/Users/shejijiang/Desktop/temp/img',
+//     });
+//     slice.remove();
+//     return `./img/${imgName}.png`;
+//   };
+// })();
 
 const layerStyle = (layer: Layer, node: INode) => {
   let style: IStyle = {};
