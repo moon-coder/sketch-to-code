3a24742ed6f68528e769a8c6f2a412504d50c356
yangxiaodong <yangxiaodong@qianmi.com>
2019-10-22 09:53:36 +0800

    add: 添加一行如果间隔相等则合在一起的策略..

--- src/layout/row-merge.ts
+++ src/layout/row-merge.ts
@@ -17,8 +17,10 @@ import {Range} from 'istanbul-lib-coverage';
 export default function(nodes: INode[]): INode[] {
   //按y坐标 分组, 每一组中高度进行对比 如果一致则在结果中返回;
 
+  //TODO Y坐标(行投影)也X坐标(列投影)要策略的选择那个;
   let yranges = getYRange(nodes);
-  yranges = mergeEquealSplitBlock(yranges);
+  yranges = mergeSameBlock(yranges);
+  yranges = mergeSplitEqualBlock(yranges);
 
 
   //包装后还是一个节点就没必要包起来了.
@@ -39,13 +41,97 @@ class SegmentFlag {
     this.len = len;
   }
 }
+
 /**
- * 把间隔相等的节点合为一个大节点;
+ * 将间隔相等的节点合在一起.
+ * @param {RangeItem[]} yranges
+ * @returns {RangeItem[]}
+ */
+export function mergeSplitEqualBlock(yranges: RangeItem[]): RangeItem[] {
+
+  //把间隔相等的节点合为一个大节点;
+  //从大到小排
+  yranges = yranges.sort((a, b) => a.min - b.min);
+
+  if (yranges.length === 1 || yranges.length===2) {
+    return yranges;
+  }
+
+  //间距记录;
+
+  let segmentQuene: any[]= [];
+
+  //反向分段; 间距做为对象插入, 相同的间距合并删除;
+  segmentQuene.unshift(new SegmentFlag(0));
+
+  for (let i = yranges.length-1; i > 0; i--) {
+    let yrangeMax = yranges[i];
+    let yrangeMin = yranges[i-1];
+
+    segmentQuene.unshift(yranges[i]);
+    segmentQuene.unshift(new SegmentFlag(yrangeMax.min - yrangeMin.max));
+  }
+  segmentQuene.unshift(yranges[0]);
+  segmentQuene.unshift(new SegmentFlag(0));
+
+  //删除重复的
+  let lastLen =0, recordIndex:number[]=[];
+  for (let i = segmentQuene.length-1; i >= 0; i--) {
+    let segmentItem =segmentQuene[i];
+    if(segmentItem instanceof SegmentFlag) {
+      if(segmentItem.len ===lastLen) {
+        segmentQuene.splice(i,1);//把多的len删除
+        recordIndex.push(i);
+      } else {
+        if(recordIndex.length>2){
+          recordIndex.map(_index=>segmentQuene.splice(_index,1));
+        }
+
+        lastLen= segmentItem.len||0;
+        recordIndex = [i];
+      }
+    }
+  }
+
+  //TODO 这里可以再对比下边缘的情况, 有的间隔一样,但两个不是一样的东西. 对比两个区域相似性程度
+
+
+  let resultRangeItem: RangeItem[] = [];
+
+  let endIndex= segmentQuene.length;
+
+  for (let i = segmentQuene.length-1; i >= 0; i--) {
+
+    let segmentItem =segmentQuene[i];
+    if(segmentItem instanceof SegmentFlag) {
+      let beginIndex= i;
+
+      let rangesArea  = segmentQuene.slice(beginIndex,endIndex)
+        .filter(item=>!(item instanceof SegmentFlag));
+
+      if(rangesArea.length>=2) {
+        let nodes  = rangesArea.map((item:RangeItem)=>calcBoundaryNode(item.items));
+        let newBlockNodes = calcBoundaryNode(nodes);
+        newBlockNodes.style.flexDirection = 'column';
+        //TODO 要把间隔的长度记录下来吗?
+
+        resultRangeItem.unshift(new RangeItem(newBlockNodes));
+      }else if(rangesArea.length===1) {
+        resultRangeItem.unshift(rangesArea[0]);
+      }
+      endIndex=i;
+    }
+  }
+  return resultRangeItem;
+}
+
+/**
+ * 把同类节点[高度相等,元素一样]合并合为一个大节点;
  * 画图最好理解
  * @param {RangeItem[]} yranges
  * @returns {RangeItem[]}
  */
-export function mergeEquealSplitBlock(yranges: RangeItem[]): RangeItem[] {
+export function mergeSameBlock(yranges: RangeItem[]): RangeItem[] {
 
   //把间隔相等的节点合为一个大节点;
   //从大到小排
