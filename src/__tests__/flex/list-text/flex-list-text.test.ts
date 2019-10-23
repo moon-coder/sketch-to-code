2cbc71801f888308355b9a48d4e956836fb793be
高目维 <gaomuwei@qianmi.com>
2019-10-23 17:26:42 +0800

    add：添加flex测试用例

---
+++ src/__tests__/flex/list-text/flex-list-text.test.ts
@@ -0,0 +1,30 @@
+import {INode} from "../../../types";
+import getNodes from "../../../get-nodes";
+import layout from "../../../layout";
+import {readJSONSync} from 'fs-extra';
+
+import {join} from 'path';
+import {toJSON} from "../../../util";
+
+jest.mock('../../../outer/sketch');
+
+/**
+ * @desc
+ *
+ * @使用场景
+ *
+ * @coder.yang2010@gmail.com
+ * @Date    2019/10/23
+ **/
+
+it('list-origin', function () {
+  let layers = readJSONSync(join(__dirname, "origin.json"));
+  const nodes: INode[] = getNodes(layers.layers[0]);
+
+  // 布局处理
+  const node: INode = layout(nodes);
+
+  const result = toJSON(node);
+
+  expect(result).toMatchSnapshot("list-origin");
+});
